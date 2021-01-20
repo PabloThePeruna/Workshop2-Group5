59ef1db89ee2af9d99514958a91fd3ca209cd674
PabloThePeruna <eveliina.lehtinen@edu.turkuamk.fi>
2021-01-20 14:25:13 +0200

    Simppeliä menoa
    Lisäsin pallon joka osuessaan neliöön antaa pisteen

---
+++ Workshop2_Group5_UnityProject/Assets/PostiPeli/Scripts/DetectCollisions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+
+    private  GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Tuhoaa pallon kun se osuu neliöön ja antaa pisteen
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Kirje")
+            Destroy(collision.gameObject);
+            gameManager.UpdateScore(1);
+            Debug.Log("33733101 Tampere 10");
+    }
+}
