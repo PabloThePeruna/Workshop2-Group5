11e22153dbf785e4684ab6c24e1ddebfcc0849d2
PabloThePeruna <eveliina.lehtinen@edu.turkuamk.fi>
2021-01-20 16:50:09 +0200

    Ajastus
    Laitin ajastimen, niin että postia putoaa torvesta 5 sekunnin välein. SItä aikaa pystyy muokkaamaan jos klikkaa SpawnManageria Hierarkiasta ja vaihtaa numeroa siihen Spawn Interval kohtaan

--- Workshop2_Group5_UnityProject/Assets/PostiPeli/Scripts/SpawnManager.cs
+++ Workshop2_Group5_UnityProject/Assets/PostiPeli/Scripts/SpawnManager.cs
@@ -7,19 +7,28 @@ public class SpawnManager : MonoBehaviour
 
     public GameObject[] postiPrefabs;
 
+    // kuinka pitkään pitää odotella että torvesta alkaa tursuamaan
+    public float startDelay = 2;
+
+    //Kuinka pitkä aika on kahden tursauksen välillä, tällä hetkellä se on 5 sekunttia
+    public float spawnInterval = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomPosti", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int postiIndex = Random.Range(0, postiPrefabs.Length);
-            Instantiate(postiPrefabs[postiIndex], new Vector3(0, 4, 0), postiPrefabs[postiIndex].transform.rotation);
-        }
+        
+    }
+
+    // Turauttaa torvesta tulemaan jonkun kolmesta posti typpistä (kirjeen, kortin tai paketin)
+    void SpawnRandomPosti()
+    {
+        int postiIndex = Random.Range(0, postiPrefabs.Length);
+        Instantiate(postiPrefabs[postiIndex], new Vector3(0, 4, 0), postiPrefabs[postiIndex].transform.rotation);
     }
 }
